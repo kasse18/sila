e91294ea67b958a000eadc9d797840f705986573
pollymo <pollynaru@gmail.com>
2024-10-26 19:42:06 +0300

    login written

--- internal/delivery/handlers/container.go
+++ internal/delivery/handlers/container.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"io"
 	"net/http"
 	"sila-app/internal/models/models"
@@ -120,6 +122,37 @@ func (h ContainerHandler) Upload(g *gin.Context) {
 	})
 }
 
-func (h ContainerHandler) Login(g *gin.Context) {
+type LoginRequest struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
 
+func (h ContainerHandler) Login(g *gin.Context) {
+	var request LoginRequest
+	login, password := "123", "123"
+	if err := g.ShouldBindJSON(&request); err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if request.Login != login || request.Password != password {
+		g.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Login and password are incorrect")})
+		return
+	}
+	secretKey := []byte("your_secret_key_here")
+
+	claims := jwt.MapClaims{
+		"login": login,
+		"exp":   0,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	g.JSON(http.StatusOK, gin.H{
+		"message": "Token generated successfully",
+		"token":   tokenString,
+	})
 }
