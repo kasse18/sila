ceee30fbdbed1a07a69447c086585fb98bbdc1a1
Artem Belyaev <117168259+kasse18@users.noreply.github.com>
2024-10-27 10:52:38 +0300

    Update cors.go

--- internal/delivery/handlers/middleware/cors.go
+++ internal/delivery/handlers/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func (m Middleware) CORSMiddleware() gin.HandlerFunc {
 	return func(g *gin.Context) {
 		g.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		g.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:9002")
+		g.Writer.Header().Set("Access-Control-Allow-Origin", "http://90.156.229.249:9002")
 		g.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		g.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		g.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
